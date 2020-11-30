c38b45e8f5b606b8a50a5d35f07aae170ebc561b
Michael Ryan <ryanabu@bc.edu>
2020-11-30 10:48:38 -0500

    Initial Commit

---
+++ FairwayFinder/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FairwayFinder
+//
+//  Created by Michael Ryan on 11/30/20.
+//  Copyright © 2020 Michael Ryan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
